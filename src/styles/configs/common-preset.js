4fad0ff4b81a276f5efa855edafd5cdb12ea5bee
sivasaikrishna7 <sivasaikrishna.n18@iiits.in>
2022-05-31 22:13:51 +0530

    commit

---
+++ src/styles/configs/common-preset.js
@@ -0,0 +1,14 @@
+const screens = require("./screens");
+
+module.exports = {
+  theme: {
+    screens: screens,
+    fontFamily: {
+      lato: ['"Lato"'],
+      "lato-bold": ['"Lato-Bold"'],
+      "lato-italic": ['"Lato-Italic"'],
+      "lato-black": ['"Lato-Black"'],
+    },
+  },
+  important: true,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  presets: [require("./src/styles/configs/common-preset.js")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
